7948049dc2cb8fbf90ff7edba9599a0a2e2cdcb8
shivaraj-bh <me@shivaraj-bh.in>
2024-09-09 16:03:01 +0530

    ci: Add `cabal-run` custom step in om config

--- nix/om.nix
+++ nix/om.nix
@@ -6,10 +6,17 @@
       steps = {
         # Users can define custom steps to run any arbitrary flake app or devShell command.
         custom = {
-          # This equivalent to `nix run .#hs-cac-client`
+          # This is equivalent to `nix run .#hs-cac-client`
           hello-haskell = {
             type = "app";
             name = "hello-haskell";
+          };
+          # This is equivalent to `nix develop .#default -c cd haskell && cabal run`
+          cabal-run = {
+            type = "devshell";
+            # Name of the devshell profile to use
+            # name = "default";
+            command = "cd haskell && cabal run";
           };
         };
       };
